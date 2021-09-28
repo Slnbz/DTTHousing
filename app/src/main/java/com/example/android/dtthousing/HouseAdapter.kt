9556c4fc15fa351a3d5141b9624b1c8db98e1cb9
Slnbz <selenbozbyk@yahoo.com.tr>
2021-09-28 14:17:34 +0200

    definition fixes

--- app/src/main/java/com/example/android/dtthousing/HouseAdapter.kt
+++ app/src/main/java/com/example/android/dtthousing/HouseAdapter.kt
@@ -1,13 +1,11 @@
 package com.example.android.dtthousing
 
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
