8b3f12a47afe0cd78f7a9eb1d8ad8535f1a275fb
Slnbz <selenbozbyk@yahoo.com.tr>
2021-09-28 13:55:10 +0200

    setting up new Activity when itemClicked

--- app/src/main/java/com/example/android/dtthousing/MainActivity.kt
+++ app/src/main/java/com/example/android/dtthousing/MainActivity.kt
@@ -54,7 +54,12 @@ class MainActivity : AppCompatActivity() {
     private fun showHouses(houses: List<House>){
         val recyclerViewHouses: RecyclerView = findViewById(R.id.recyclerview_houses)
         recyclerViewHouses.layoutManager = LinearLayoutManager(this)
-        recyclerViewHouses.adapter = HouseAdapter(houses,)
+        recyclerViewHouses.adapter = HouseAdapter(houses){ house ->
+            val houseDetailsActivityIntent = Intent(this, HouseDetails::class.java)
+            houseDetailsActivityIntent.putExtra("latitude", house.latitude)
+            houseDetailsActivityIntent.putExtra("longitude", house.longitude)
+            startActivity(houseDetailsActivityIntent)
+        }
     }
 
 
